7c97eae04101880eac8464bc56cb78f2b7c0de7f
mrperfect <mrperfect123@gmail.com>
2021-09-12 06:41:07 +0530

    adding data of breaker in file

---
+++ src/breaker.java
@@ -0,0 +1,2 @@
+A circuit breaker is an electrical switch designed to protect an electrical circuit from damage caused by overcurrent/overload or short circuit. Its basic function is to interrupt current flow after protective relays detect a fault.
+
